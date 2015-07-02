b22d39c9349dd5cb120c6b99f1fe1d69beefb98e
Sandeep Kaur <deepsandhuleo@gmail.com>
2015-07-02 13:19:42 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat');
+
+//Paths
+var scssSource = 'components/scss/',
+    cssDestination = 'theme_files/css/';
+
+//Tasks
+gulp.task('sass', function () {
+    return sass(scssSource, {sourcemap: true})
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(sourcemaps.write('/maps'))
+        .pipe(gulp.dest(cssDestination));
+});
+
+//Default task and watch expression
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function() {
+  gulp.watch(scssSource + '*.scss', ['sass']);
+  gulp.watch(jsSources + '*.js', ['js']);
+})
